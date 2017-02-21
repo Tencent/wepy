33a75302a7b6e3d099794b9f139e11017213b2d8
神奇辉 <shenqihui0920@gmail.com>
2017-02-21 10:45:48 +0800

    Replace setting 数组配置方式。

--- packages/wepy-plugin-replace/src/index.js
+++ packages/wepy-plugin-replace/src/index.js
@@ -15,16 +15,33 @@ export default class {
 
         let setting = this.setting;
 
-        if (!setting.filter.test(op.file)) {
-            op.next();
-        } else {
-            op.output && op.output({
-                action: '变更',
-                file: op.file
-            });
+        let settings = [];
 
-            op.code = op.code.replace(setting.config.find, setting.config.replace);
-            op.next();
+        if (setting instanceof Array) {
+            settings = settings.concat(setting);
+        } else if (setting instanceof Object && setting['0']) {
+            for (let key in setting) {
+                let value = setting[key];
+                if(value.filter) {
+                    settings.push(value);
+                }
+            }
         }
+        else if (setting instanceof Object && setting.filter) {
+            settings.push(setting);
+        }
+
+        settings.forEach((setting) => {
+          if (setting.filter.test(op.file)) {
+              op.output && op.output({
+                  action: '变更',
+                  file: op.file
+              });
+
+              op.code = op.code.replace(setting.config.find, setting.config.replace);
+          }
+        })
+
+        op.next();
     }
 }
