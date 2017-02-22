1513a0d159fd05e1ca10b1c68e4fa40084c9dd34
神奇辉 <shenqihui0920@gmail.com>
2017-02-22 12:23:31 +0800

    pug 编译，默认 div 改成 view

--- packages/wepy-compiler-pug/src/index.js
+++ packages/wepy-compiler-pug/src/index.js
@@ -7,8 +7,10 @@ let compiler = function (content, config) {
     try {
         let fn = pug.compile(content, config);
         let html = fn(data);
+        html = (html || '').replace(/<div /ig, '<view ').replace(/<\/div>/ig, '</view>').replace(/<div>/ig, '<view>')
         p = Promise.resolve(html);
     } catch (e) {
+        console.log('编辑 PUG 错误', e);
         p = Promise.reject(e);
     }
     return p;
@@ -21,7 +23,9 @@ compiler.sync = function (content, config) {
     try {
         let fn = pug.compile(content, config);
         html = fn(data);
+        html = (html || '').replace(/<div /ig, '<view ').replace(/<\/div>/ig, '</view>').replace(/<div>/ig, '<view>')
     } catch (e) {
+      console.log('编辑 PUG 错误', e);
     }
     return html;
 };
