8bfd037af6eef26fc4c0134bba78a326d54a1700
dolymood <dolymood@gmail.com>
2017-04-13 11:27:25 +0800

    wepy-redux

---
+++ packages/wepy-redux/src/index.js
@@ -0,0 +1,13 @@
+import connect from './connect';
+import { setStore, getStore } from './store';
+import { mapState, mapMutations, mapActions } from './helpers';
+
+export {
+    connect,
+    setStore,
+    getStore,
+    mapState,
+    mapMutations,
+    // mapGetters,
+    mapActions
+}
---
+++ packages/wepy-redux/src/store.js
@@ -0,0 +1,9 @@
+let store = null
+
+export function getStore () {
+    return store;
+}
+
+export function setStore (s) {
+    store = s;
+}
