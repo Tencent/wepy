0646e168fb8fc882f8ea44d6a63b8bb493310e1a
Gcaufy <gcaufy@gmail.com>
2017-08-18 10:00:26 +0800

    fixed ci failure

--- packages/wepy-web/src/platform/wechat.js
+++ packages/wepy-web/src/platform/wechat.js
@@ -90,7 +90,7 @@ wx.scanCode = (params) => {
 
     wx.scanQRCode({
         needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
-        scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
+        scanType: ['qrCode', 'barCode'], // 可以指定扫二维码还是一维码，默认二者都有
         success (res) {
             if (res.resultStr.includes(EAN_13)) {
                 wxSuccess('scanCode', params, {
