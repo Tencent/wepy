c0b111ba0c8b9c08d29047eed1a8d3ea43d2e735
duanlian <505441575@qq.com>
2017-12-03 00:45:23 +0800

    wepy-cli: Added aliasFields feature && mainFields feature

--- packages/wepy-cli/src/resolve.js
+++ packages/wepy-cli/src/resolve.js
@@ -10,17 +10,67 @@
 
 import path from 'path';
 import util from './util';
+import cache from './cache'
+
+const DEFAULT_MODULES = ['node_modules'];
+const DEFAULT_ALIASFIELDS = ['wepy', 'weapp', 'browser'];
+const DEFAULT_MAINFIELDS = ['wepy', 'weapp', 'browser', 'module', 'main'];
 
 export default {
     init (config) {
-        this.modules = config.modules || ['node_modules'];
         this.alias = config.alias;
-
-        if (typeof this.modules === 'string') {
-            this.modules = [this.modules];
-        }
-
-        let cwd = process.cwd();
+        this.modules = config.modules || DEFAULT_MODULES;
+        this.aliasFields = config.aliasFields || DEFAULT_ALIASFIELDS;
+        this.mainFields = config.mainFields || DEFAULT_MAINFIELDS;
+        
+        ['modules', 'aliasFields', 'mainFields'].forEach(opt => {
+            typeof this[opt] === 'string' && (
+                this[opt] = [].concat(this[opt])
+            );
+        });
+  
+        let pkgFile = util.getPkg();
+        let pkg = JSON.parse(pkgFile);
+        let cwd = util.currentDir;
+        let ext = cache.getExt();
+        
+        // 优先级递减
+        this.aliasFields.forEach(fields => {
+            // 归类
+            util.isObject(pkg[fields]) && Object.keys(pkg[fields] || {}).forEach(key => {
+                // module形式的fieldsAlias归置于alias中，例： "xyz": "./src/xyz.js"，alias优先级较大
+                if (key.indexOf('.') === -1) {
+                    // => "xyz"、"xyz-xyz"
+                    let value;
+                    if (!pkg[fields][key]) {
+                        value = 'false'
+                    } else {
+                        value = path.resolve(cwd, pkg[fields][key])
+                    }
+                  
+                    // fields中key或value路径后缀与配置缺省值相同时，replace后缀
+                    if (path.extname(value) === ext)
+                        value = value.replace(ext, '')
+                  
+                    this.alias = Object.assign({}, { [key]: value }, this.alias || {});
+                }
+                // fields转换："./src/index.wpy" => "src/index.wpy"后合并至fieldsAlias中
+                if (!path.isAbsolute(key) && path.extname(key)) {
+                    // => "./src/xyz.js"、"src/xyz.js"
+                    let value = path.resolve(cwd, pkg[fields][key])
+                    key = path.resolve(cwd, key)
+                  
+                    // fields中key或value路径后缀与配置缺省值相同时，replace后缀
+                    if (path.extname(key) === ext)
+                        key = key.replace(ext, '')
+                  
+                    if (path.extname(value) === ext)
+                        value = value.replace(ext, '')
+                  
+                    this.fieldsAlias = Object.assign({}, { [key]: value }, this.fieldsAlias || {});
+                }
+            });
+        });
 
         this.modulePaths = this.modules.map(v => {
             if (path.isAbsolute(v)) {
@@ -95,10 +145,19 @@ export default {
         if (!o) {
             return null;
         }
-        let main = o.pkg.main || 'index.js';
-        if (o.pkg.browser && typeof o.pkg.browser === 'string') {
-            main = o.pkg.browser;
+
+        // 优先级递减
+        let mainField, main
+        for (let i = 0, l = this.mainFields.length; i < l; i++) {
+            mainField = this.mainFields[i];
+            if (o.pkg[mainField] && typeof o.pkg[mainField] === 'string') {
+                main = o.pkg[mainField];
+                break;
+            }
         }
+
+        main = main || 'index.js';
+
         return {
             file: main,
             modulePath: o.modulePath,
@@ -106,14 +165,53 @@ export default {
             dir: o.dir
         };
     },
+  
+    resolveFieldsAlias (lib) {
+        return lib && this.fieldsAlias && this.fieldsAlias[lib]
+            ? this.fieldsAlias[lib]
+            : lib;
+    },
+  
+    replaceFieldsAlias (currentAlias, opath) {
+        let absolutePath;
+        
+        if (path.isAbsolute(currentAlias)) {
+            absolutePath = currentAlias
+          
+            currentAlias = this.resolveFieldsAlias(absolutePath) !== absolutePath
+                ? this.resolveFieldsAlias(absolutePath)
+                : currentAlias;
+        } else if (currentAlias[0] === '.') {
+            absolutePath = path.join(opath.dir, currentAlias);
+            
+            currentAlias = this.resolveFieldsAlias(absolutePath) !== absolutePath
+                ? this.resolveFieldsAlias(absolutePath)
+                : currentAlias;
+        } else if (
+            currentAlias.indexOf('/') === -1 || // require('asset');
+            currentAlias.indexOf('/') === currentAlias.length - 1 || // require('a/b/something/')
+            (currentAlias[0] === '@' && currentAlias.indexOf('/') !== -1 && currentAlias.lastIndexOf('/') === currentAlias.indexOf('/')) // require('@abc/something')
+        ) {
+            const mainFile = this.getMainFile(currentAlias);
+            
+            if (mainFile) {
+                absolutePath = path.join(mainFile.dir, mainFile.file);
+                currentAlias = this.resolveFieldsAlias(absolutePath) !== absolutePath
+                    ? this.resolveFieldsAlias(absolutePath)
+                    : currentAlias;
+            }
+        }
+        return currentAlias;
+    },
 
-    resolveAlias (lib) {
+    resolveAlias (lib, opath) {
         if (!this.alias)
             return lib;
         if (this._cacheAlias[lib]) {
             return this._cacheAlias[lib];
         }
         let rst = lib;
+        let ext = cache.getExt();
 
         for (let k in this.alias) {
             let alias = this.alias[k];
@@ -135,6 +233,8 @@ export default {
         if (!this._cacheAlias[lib]) {
             this._cacheAlias[lib] = lib;
         }
+        // replace field alias
+        this._cacheAlias[lib] = this.replaceFieldsAlias(this._cacheAlias[lib], opath)
         return this._cacheAlias[lib];
     }   
 }
