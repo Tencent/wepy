3f08759c7047bae1244496aea9d431b9f8410925
Gcaufy <gcaufy@gmail.com>
2017-12-13 14:03:43 +0800

    added jsonlint for docs files

--- scripts/test.sh
+++ scripts/test.sh
@@ -11,5 +11,8 @@ if [ "$TEST_DEBUG" ]; then
    node="node --inspect --debug-brk"
 fi
 
+jsonlint -q ./docs/data/cases.json && echo 'cases.json ok.'
+jsonlint -q ./docs/data/donate.json && echo 'donate.json ok.'
+
 eslint packages/*
 istanbul cover ./node_modules/mocha/bin/_mocha -- -t 50000 --recursive  -R spec packages/*/test/
