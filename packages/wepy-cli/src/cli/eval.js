f87d3cff55d5036bf78c65cc463265164a878b60
duanlian <505441575@qq.com>
2018-01-23 16:57:36 +0800

    wepy-cli: added wepy init && wepy list commander

---
+++ packages/wepy-cli/src/cli/eval.js
@@ -0,0 +1,15 @@
+import chalk from 'chalk';
+
+/**
+ * Evaluate an expression in meta.json in the context of
+ * prompt answers data.
+ */
+
+export default function evaluate (exp, data) {
+    const fn = new Function('data', 'with (data) { return ' + exp + '}');
+    try {
+        return fn(data);
+    } catch (e) {
+        console.error(chalk.red('Error when evaluating filter condition: ' + exp));
+    }
+}
