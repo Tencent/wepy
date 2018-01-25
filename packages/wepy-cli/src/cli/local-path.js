a7bccfefe2f22e38be9ccc6c08975fc63f34d201
Gcaufy <gcaufy@gmail.com>
2018-01-25 16:40:22 +0800

    fix indent

--- packages/wepy-cli/src/cli/local-path.js
+++ packages/wepy-cli/src/cli/local-path.js
@@ -2,16 +2,13 @@ import path from 'path';
 
 export default {
     isLocalPath (templatePath) {
-    // templatePath example:
-    // .wepy_templates
-    // E:\workspace\wepy_templates\standard
+        // templatePath example:
+        // .wepy_templates
+        // E:\workspace\wepy_templates\standard
         return /^[./]|(^[a-zA-Z]:)/.test(templatePath);
     },
     getTemplatePath (templatePath) {
         return path.isAbsolute(templatePath)
-      ? templatePath
-      : path.normalize(
-        path.join(process.cwd(), templatePath)
-      );
+            ? templatePath : path.normalize(path.join(process.cwd(), templatePath));
     }
 };
