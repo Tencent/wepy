6f4145941d84feb763dc31009253c929f9975908
Gcaufy <gcaufy@gmail.com>
2018-01-25 16:40:22 +0800

    fix indent

--- packages/wepy-cli/src/cli/options.js
+++ packages/wepy-cli/src/cli/options.js
@@ -1,8 +1,8 @@
-import path from 'path';
-import metadata from 'read-metadata';
-import { existsSync as exists } from 'fs';
-import getGitUser from './git-user';
-import validateName from 'validate-npm-package-name';
+import path from 'path'
+import metadata from 'read-metadata'
+import { existsSync as exists } from 'fs'
+import getGitUser from './git-user'
+import validateName from 'validate-npm-package-name'
 
 /**
  * Read prompts metadata.
@@ -12,17 +12,17 @@ import validateName from 'validate-npm-package-name';
  */
 
 export default function options (name, dir) {
-    const opts = getMetadata(dir);
+    const opts = getMetadata(dir)
 
-    setDefault(opts, 'name', name);
-    setValidateName(opts);
+    setDefault(opts, 'name', name)
+    setValidateName(opts)
 
-    const author = getGitUser();
+    const author = getGitUser()
     if (author) {
-        setDefault(opts, 'author', author);
+        setDefault(opts, 'author', author)
     }
 
-    return opts;
+    return opts
 }
 
 /**
@@ -33,21 +33,21 @@ export default function options (name, dir) {
  */
 
 function getMetadata (dir) {
-    const json = path.join(dir, 'meta.json');
-    const js = path.join(dir, 'meta.js');
-    let opts = {};
+    const json = path.join(dir, 'meta.json')
+    const js = path.join(dir, 'meta.js')
+    let opts = {}
 
     if (exists(json)) {
-        opts = metadata.sync(json);
+        opts = metadata.sync(json)
     } else if (exists(js)) {
-        const req = require(path.resolve(js));
+        const req = require(path.resolve(js))
         if (req !== Object(req)) {
-            throw new Error('meta.js needs to expose an object');
+            throw new Error('meta.js needs to expose an object')
         }
-        opts = req;
+        opts = req
     }
 
-    return opts;
+    return opts
 }
 
 /**
@@ -60,30 +60,30 @@ function getMetadata (dir) {
 
 function setDefault (opts, key, val) {
     if (opts.schema) {
-        opts.prompts = opts.schema;
-        delete opts.schema;
+        opts.prompts = opts.schema
+        delete opts.schema
     }
-    const prompts = opts.prompts || (opts.prompts = {});
+    const prompts = opts.prompts || (opts.prompts = {})
     if (!prompts[key] || typeof prompts[key] !== 'object') {
         prompts[key] = {
             'type': 'string',
             'default': val
-        };
+        }
     } else {
-        prompts[key]['default'] = val;
+        prompts[key]['default'] = val
     }
 }
 
 function setValidateName (opts) {
-    const name = opts.prompts.name;
-    const customValidate = name.validate;
+    const name = opts.prompts.name
+    const customValidate = name.validate
     name.validate = name => {
-        const its = validateName(name);
+        const its = validateName(name)
         if (!its.validForNewPackages) {
-            const errors = (its.errors || []).concat(its.warnings || []);
-            return 'Sorry, ' + errors.join(' and ') + '.';
+            const errors = (its.errors || []).concat(its.warnings || [])
+            return 'Sorry, ' + errors.join(' and ') + '.'
         }
-        if (typeof customValidate === 'function') return customValidate(name);
-        return true;
-    };
+        if (typeof customValidate === 'function') return customValidate(name)
+        return true
+    }
 }
