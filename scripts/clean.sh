7da2dfd256c64b784918cfff7844b5f202d83bde
Gcaufy <gcaufy@gmail.com>
2018-01-29 10:37:59 +0800

    added clean scripts

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -11,6 +11,10 @@ if [ "$TEST_DEBUG" ]; then
    node="node --inspect --debug-brk"
 fi
 
-
+rm -rf node_modules
+rm -rf yarn.lock
+rm -rf package-lock.json
 rm -rf packages/*/node_modules
 rm -rf packages/*/lib
+rm -rf packages/*/yarn.lock
+rm -rf packages/*/package-lock.json
