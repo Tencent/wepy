4942f45b089940971c2cb02acc600cc14c178e4d
Gcaufy <gcaufy@gmail.com>
2018-02-01 14:56:39 +0800

    fixed style link empty bug

--- packages/wepy-cli/src/compile-wpy.js
+++ packages/wepy-cli/src/compile-wpy.js
@@ -1,7 +1,7 @@
 /**
  * Tencent is pleased to support the open source community by making WePY available.
  * Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
- * 
+ *
  * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
@@ -176,7 +176,7 @@ export default {
 
                 if (rstTypeObj.src) {
                     // rstTypeObj.src = path.resolve(opath.dir, rstTypeObj.src);
-                    rstTypeObj.src = resolve.resolveAlias(rstTypeObj.src, opath);
+                    rstTypeObj.src = path.resolve(opath.dir, resolve.resolveAlias(rstTypeObj.src, opath));
                     rstTypeObj.link = true;
                 } else {
                     rstTypeObj.link = false;
@@ -524,7 +524,7 @@ export default {
                     } else {
                         requires.push(path.join(opath.dir, wpy.template.components[k]));
                     }
-                    
+
                     // 去重
                     // Example:
                     // components = {
