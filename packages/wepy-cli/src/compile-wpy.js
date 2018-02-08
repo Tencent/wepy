5c623d8eeed5f6221fa1454b5be0d8cee848dd16
Gcaufy <gcaufy@gmail.com>
2018-02-08 17:06:09 +0800

    rollback scoped changes

--- packages/wepy-cli/src/compile-wpy.js
+++ packages/wepy-cli/src/compile-wpy.js
@@ -559,13 +559,13 @@ function walkNode (node, moduleId) {
     if (node.childNodes) {
         [].slice.call(node.childNodes || []).forEach((child) => {
             if (child.tagName) {
-                if (child.hasAttribute('class') || child.hasAttribute('v-bind:class.once') || child.hasAttribute(':class')) {
+                // scoped 情况下，可能直接把样式绑定到节点上。
+                // if (child.hasAttribute('class') || child.hasAttribute('v-bind:class.once') || child.hasAttribute(':class')) {
                     // 是标签 则增加class
                     const cls = child.getAttribute('class');
                     child.setAttribute('class', (cls + ' ' + moduleId).trim());
-                    walkNode(child, moduleId);
-
-                }
+                // }
+                walkNode(child, moduleId);
             }
         });
     }
