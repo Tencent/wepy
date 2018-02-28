91d1cd32f074a46de028523d30616fd0d0e3cb53
casa <cuijiemmx@163.com>
2018-03-01 02:06:05 +0800

    remove useless exports from wepy-redux

--- packages/wepy-redux/src/index.js
+++ packages/wepy-redux/src/index.js
@@ -10,12 +10,9 @@
 
 import connect from './connect';
 import { setStore, getStore } from './store';
-import { mapState, mapActions } from './helpers';
 
 export {
     connect,
     setStore,
-    getStore,
-    mapState,
-    mapActions
+    getStore
 }
