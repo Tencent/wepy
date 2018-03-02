6a3d6b5debc71ec0629ea9d8dad00178a3a13cfb
pubby <ruanchunping@gmail.com>
2018-03-02 22:50:46 +0800

    修复repeat组件for="{{[var1,var2]}}"时的数据绑定问题

--- packages/wepy/src/component.js
+++ packages/wepy/src/component.js
@@ -1,7 +1,7 @@
 /**
  * Tencent is pleased to support the open source community by making WePY available.
  * Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
- * 
+ *
  * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
@@ -148,7 +148,7 @@ export default class {
                             props[key].index = binded.index;
                             props[key].key = binded.key;
                             props[key].value = binded.value;
-                            
+
                             inRepeat = true;
 
                             let bindfor = binded.for, binddata = $parent;
@@ -320,9 +320,26 @@ export default class {
                     if (binded) {
                         if (typeof(binded) === 'object') {
                             let bindfor = binded.for, binddata = $parent;
-                            bindfor.split('.').forEach(t => {
-                                binddata = binddata ? binddata[t] : {};
-                            });
+
+                            // 处理 for="{{[var1,var2]}}"
+                            if (bindfor.indexOf('[') === 0) {
+                                let bdarr = [];
+                                bindfor = bindfor.substr(1, bindfor.length - 2).trim()
+
+                                bindfor.split(',').forEach(function (e) {
+                                    let bd = $parent;
+                                    e.trim().split('.').forEach(function (t) {
+                                        bd = bd ? bd[t] : {};
+                                    })
+                                    bdarr.push(bd)
+                                })
+
+                                binddata = bdarr
+                            } else {
+                                bindfor.split('.').forEach(t => {
+                                    binddata = binddata ? binddata[t] : {};
+                                });
+                            }
 
                             index = Array.isArray(binddata) ? +index : index;
 
@@ -566,7 +583,7 @@ export default class {
                         }
                     }
                     // Send to ReadyToSet
-                    readyToSet[this.$prefix + k] = this[k]; 
+                    readyToSet[this.$prefix + k] = this[k];
                     this.data[k] = this[k];
                     originData[k] = util.$copy(this[k], true);
                     if (this.$repeat && this.$repeat[k]) {
