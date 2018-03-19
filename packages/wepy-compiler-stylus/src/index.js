003597ca812944b76a047eec8ae1ccff818712fb
Gcaufy <gcaufy@gmail.com>
2018-03-19 12:24:38 +0800

    added stylus deps support

--- packages/wepy-compiler-stylus/src/index.js
+++ packages/wepy-compiler-stylus/src/index.js
@@ -1,7 +1,7 @@
 /**
  * Tencent is pleased to support the open source community by making WePY available.
  * Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
- * 
+ *
  * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
@@ -17,10 +17,26 @@ export default function (content, config, file) {
         config.paths = [opath.dir];
         config.filename = opath.base;
 
-        stylus.render(content, config, function (err, css) {
+        let instance = stylus(content);
+
+        for (let k in config) {
+            instance.set(k, config[k]);
+        }
+        let imports = instance.deps();
+
+        instance.render(function (err, css) {
             if (err) reject(err);
             else {
-                resolve(css);
+                // Only in 1.7.2, this option is true
+                if (config.supportObject) {
+                    let result = {};
+                    result.css = css;
+                    result.context = file;
+                    result.imports = imports;
+                    resolve(result);
+                } else {
+                    resolve(css);
+                }
             }
         });
     });
