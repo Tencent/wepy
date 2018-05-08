75746b2654dc0ecafec100a08f8a25e099e59891
Gcaufy <gcaufy@gmail.com>
2018-05-08 16:07:36 +0800

    code update

--- packages/wepy-cli/core/parseTemplate.js
+++ packages/wepy-cli/core/parseTemplate.js
@@ -6,17 +6,38 @@
  * http://opensource.org/licenses/MIT
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
-const sfcCompiler = require('vue-template-compiler');
 const fs = require('fs');
 const path = require('path');
+const htmlparser = require('htmlparser2');
 const parseOptions = require('./parseOptions');
+
 
 const ENTRY_FILE = 'app.wpy';
 
 exports = module.exports =  {
 
-  parse (file) {
-    return Promise.resolve(file);
-  }
+  parse (compiled, ctx) {
+    return this.getAST(compiled.content).then(ast => {
+      debugger;
 
+      return compiled;
+    }).catch(e => {
+      throw e;
+    })
+  },
+
+  getAST (html) {
+    return new Promise((resolve, reject) => {
+      const handler = new htmlparser.DomHandler(function (error, dom) {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(dom);
+        }
+      });
+      const parser = new htmlparser.Parser(handler);
+      parser.write(html);
+      parser.end();
+    });
+  }
 }
