8a8464fc3f6e9f8024ba964da7b95553d310e69f
Jesse <xinming.feng@tongdun.cn>
2018-07-26 17:35:45 +0800

    issues#1544

--- packages/wepy-compiler-pug/README.md
+++ packages/wepy-compiler-pug/README.md
@@ -1,22 +1,66 @@
-# wepy pug(jade) 编译器
+# wepy pug/jade编译插件
 
-## 安装
+[![npm package](https://nodei.co/npm/wepy-compiler-pug.png?downloads=true&downloadRank=true&stars=true)](https://www.npmjs.com/package/wepy-compiler-pug)
+
+> Note: 为了更好的使用pug开发项目，迁移来自[https://github.com/fengxinming/wepy-compiler-pug](https://github.com/fengxinming/wepy-compiler-pug)。
+
+---
+
+<p align="center">
+  <a href="https://tencent.github.io/wepy/">
+    <img alt="WePY" src="http://sem.g3img.com/g3img/zhongshihudong/c2_20170623114249_41503.png" width="210"/>
+  </a>
+</p>
+
+## Table of contents
+
+  - [Features](#features)
+  - [Installation](#installation)
+  - [Usage](#usage)
+
+<br/>
+
+## Features
+
+### Supported template engines
+
+  - [jade](https://github.com/visionmedia/jade) [(website)](http://jade-lang.com/)
+  - [pug (formerly jade)](https://github.com/pugjs/pug) [(website)](https://pugjs.org)
+
+## installation
+
+```bash
+cnpm install wepy-compiler-jade --save-dev
+
+# or
+
+npm install wepy-compiler-jade --save-dev
 
 ```
-npm install wepy-compiler-pug --save-dev
-```
 
-## 配置`wepy.config.js`
+
+## Usage
 
 ```
+// configure wepy.config.js
+
 module.exports = {
-    "compilers": {
-        pug: {
-        }
+  compilers: {
+    jade: {
+      engine: 'jade',            // 默认为pug。如果需要使用 pug 模板，就在此处设置
+      enforcePretty: true,       // 默认为false，即：该参数用于模板引擎美化失效时强行美化。
+      globalConfig: {            // 这个属性名字可以随便定义，只要在模板中使用相同的名字即可
+        imgUrlPrefix: ''
+      }
     }
+  }
 };
+
+// write vue/wpy template
+
+<template lang="pug">
+  view
+    image(src=`${globalConfig.imgUrlPrefix}/images/xxx.svg`)
+</template>    
+
 ```
-
-## 参数说明
-
-[pug](https://github.com/pugjs/pug)
--- packages/wepy-compiler-pug/src/index.js
+++ packages/wepy-compiler-pug/src/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /**
  * Tencent is pleased to support the open source community by making WePY available.
  * Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
@@ -5,32 +7,65 @@
  * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ * 
+ * @modifier fengxinming
+ * 迁移来自https://github.com/fengxinming/wepy-compiler-pug
  */
 
 
-import pug from 'pug';
+const jade = require('jade');
+const pug = require('pug');
+const pretty = require('pretty');
 
-let compiler = function (content, config) {
-    let data = config.data;
-    let p;
-    delete config.data;
-    try {
-        let fn = pug.compile(content, config);
-        let html = fn(data);
-        p = Promise.resolve(html);
-    } catch (e) {
-        p = Promise.reject(e);
+const util = require('./util');
+
+const defaults = {
+    cache: false,
+    pretty: true
+};
+
+const engineSource = {
+    jade,
+    pug
+};
+
+function render(content, config) {
+    let engine = config.engine;
+    if (!engine) {
+        engine = 'pug';
     }
-    return p;
+    engine = engineSource[engine];
+    if (!engine) {
+        throw new Error(`Engine ${engine} not found`);
+    }
+    const indent = util.getIndent(content);
+    if (indent.firstLineIndent) {
+        content = util.fixIndent(content, indent.firstLineIndent * -1, indent.char);
+    }
+    config = Object.assign({}, defaults, config);
+    let html = engine.compile(content, config)(config);
+
+    // pug模板准备移除这个"pretty"属性，官方不建议使用它，
+    // 我们额外增加一个"enforcePretty"属性强行美化模板
+    if (config.enforcePretty) {
+        html = pretty(html)
+    }
+    return html;
+}
+
+function compiler(content, config) {
+    return new Promise((resolve, reject) => {
+        try {
+            let html = render(content, config);
+            resolve(html);
+        } catch (e) {
+            reject(e);
+        }
+    });
 };
 
 compiler.sync = function (content, config) {
-    let data = config.data;
-    let p, html;
-    delete config.data;
-    let fn = pug.compile(content, config);
-    html = fn(data);
-    return html;
+    return render(content, config);
 };
 
-export default compiler;
+module.exports = compiler;
