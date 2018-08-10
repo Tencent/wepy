6843148831bbaca9e8e73de39b82733aa0395c83
Gcaufy <gcaufy@gmail.com>
2018-08-10 17:06:39 +0800

    refactor(cli): rename wepy-cli to cli

--- packages/wepy-cli/templates/template/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from 'redux'
-import promiseMiddleware from 'redux-promise'
-import rootReducer from './reducers'
-
-export default function configStore () {
-  const store = createStore(rootReducer, applyMiddleware(promiseMiddleware))
-  return store
-}
