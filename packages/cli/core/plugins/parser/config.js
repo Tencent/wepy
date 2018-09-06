d7465d23c93d4e471d4b9072338cbf5ddc8d3ab5
Gcaufy <gcaufy@gmail.com>
2018-09-06 15:06:15 +0800

    feat(cli): aadded resolve support for config

--- packages/cli/core/plugins/parser/config.js
+++ packages/cli/core/plugins/parser/config.js
@@ -1,5 +1,9 @@
+const path = require('path');
+const loaderUtils = require('loader-utils');
+
+
 exports = module.exports = function () {
-  this.register('wepy-parser-config', function (rst) {
+  this.register('wepy-parser-config', function (rst, ctx) {
     let configString = rst.content.replace(/^\n*/, '').replace(/\n*$/, '');
     configString = configString || '{}';
     let config = null;
@@ -11,6 +15,31 @@ exports = module.exports = function () {
     }
     config.component = true;
     config.usingComponents = config.usingComponents || {};
-    return Promise.resolve(config);
+
+    let componentKeys = Object.keys(config.usingComponents);
+
+    if (componentKeys.length === 0) {
+      return Promise.resolve(config);
+    }
+
+    // Getting resolved path for usingComponents
+
+    let resolved = Object.keys(config.usingComponents).map(comp => {
+      const url = config.usingComponents[comp];
+      const moduleRequest = loaderUtils.urlToRequest(url, url.charAt(0) === '/' ? '' : null);
+      return this.resolvers.normal.resolve({}, path.dirname(ctx.file), moduleRequest, {}).then(rst => {
+        let parsed = path.parse(rst.path);
+        let fullpath = path.join(parsed.dir, parsed.name); // remove file extention
+        let relative = path.relative(path.dirname(ctx.file), fullpath);
+        return [comp, relative];
+      });
+    });
+
+    return Promise.all(resolved).then(rst => {
+      rst.forEach(item => {
+        config.usingComponents[item[0]] = item[1];
+      });
+      return config;
+    });
   });
 }
