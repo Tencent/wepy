71df1860e730a3726e9afd7a4ab62e07ee3e17fc
Gcaufy <gcaufy@gmail.com>
2018-09-07 10:47:06 +0800

    feat(core): expose event hub

--- packages/core/weapp/wepy.js
+++ packages/core/weapp/wepy.js
@@ -1,15 +1,22 @@
+import Base from './class/Base';
 import page from './page';
 import app from './app';
 import component from './component';
 import $global from './global';
 import { use } from './apis/index';
 
-export default {
-  component: component,
-  page: page,
-  app: app,
+
+let wepy = Base;
+
+Object.assign(wepy, {
+  component,
+  page,
+  app,
   global: $global,
 
   // global apis
-  use: use
-}
+  use
+});
+
+
+export default wepy;
