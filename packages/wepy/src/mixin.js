976e0fd085ff1580d17cc56be25733f16c65d96c
Gcaufy <gcaufy@gmail.com>
2018-09-18 21:55:52 +0800

    fix(wepy): added methods support in mixin

--- packages/wepy/src/mixin.js
+++ packages/wepy/src/mixin.js
@@ -1,7 +1,7 @@
 /**
  * Tencent is pleased to support the open source community by making WePY available.
  * Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
- * 
+ *
  * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
@@ -34,10 +34,16 @@ export default class {
 
 
         // 数据，计算属性，事件，组件覆盖
-        ['data', 'computed', 'events', 'components'].forEach((item) => {
+        ['data', 'computed', 'events', 'components', 'methods'].forEach((item) => {
             Object.getOwnPropertyNames(this[item]).forEach((k) => {
-                if (k !== 'init' && !parent[item][k])
-                    parent[item][k] = this[item][k];
+                if (k !== 'init') {
+                    if (!parent[item]) {
+                        parent[item] = {};
+                    }
+                    if (!parent[item][k]) {
+                        parent[item][k] = this[item][k];
+                    }
+                }
             });
         });
     }
