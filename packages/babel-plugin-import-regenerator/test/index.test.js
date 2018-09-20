7649a156bc51669dc55eb56aa3fd5c5219d15d87
duanlian <505441575@qq.com>
2018-09-21 02:13:21 +0800

    test(babel-plugin-import-regenerator): added fixture tests

---
+++ packages/babel-plugin-import-regenerator/test/index.test.js
@@ -0,0 +1,28 @@
+const { transformFileSync, transform } = require('@babel/core');
+const { readdirSync, readFileSync, writeFileSync } = require('fs');
+const { join } = require('path');
+const plugin = require('../index');
+const expect = require('chai').expect;
+
+describe('babel-plugin-import-regenerator', () => {
+  let fixturesDir = join(__dirname, 'fixtures');
+  let fixtures = readdirSync(fixturesDir);
+
+  fixtures.forEach(caseName => {
+    fixturesDir = join(fixturesDir, caseName);
+    const actualFile = join(fixturesDir, 'actual.js');
+    const expectedFile = join(fixturesDir, 'expected.js');
+
+    it(`should work with ${caseName.split('-').join(' ')}`, () => {
+      actual = transform(readFileSync(actualFile), {
+        presets: [
+          'env'
+        ],
+        plugins: [plugin]
+      }).code
+
+      const expected = readFileSync(expectedFile, 'utf-8');
+      expect(actual.trim()).to.equal(expected.trim());
+    });
+  });
+});
