95ea5a2c2c894f2278e66db5fbf9b8cc5e8b1c37
Gcaufy <gcaufy@gmail.com>
2018-09-20 22:02:13 +0800

    chore(babel): upgrade babel core to v7

--- packages/compiler-babel/index.js
+++ packages/compiler-babel/index.js
@@ -7,7 +7,7 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
-const babel = require('babel-core');
+const babel = require('@babel/core');
 
 exports = module.exports = function (options) {
   return function () {
