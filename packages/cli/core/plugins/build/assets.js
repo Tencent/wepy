a207e57a45a9f91641b2a45ee8e66afe84440d2c
Gcaufy <gcaufy@gmail.com>
2018-10-18 14:28:05 +0800

    fix(cli): fixed url assets dep error

--- packages/cli/core/plugins/build/assets.js
+++ packages/cli/core/plugins/build/assets.js
@@ -14,7 +14,10 @@ exports = module.exports = function () {
     [].concat(requires, urls).forEach(file => {
 
       let fileData = this.assets.data(file);
-      this.hookSeq('script-dep-fix', fileData);
+
+      if (fileData.type !== 'url') {
+        this.hookSeq('script-dep-fix', fileData);
+      }
       let targetFile = this.getTarget(file);
 
       result.push({
