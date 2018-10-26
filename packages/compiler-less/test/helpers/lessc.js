31ae2f0f01b37f23120879a0835e8dce08981f7f
huixisheng <469184346@qq.com>
2018-10-26 17:03:31 +0800

    feat(compiler-less): add tests

--- packages/compiler-less/test/helpers/lessc.js
+++ packages/compiler-less/test/helpers/lessc.js
@@ -1,6 +1,7 @@
 const { exec } = require('child_process');
 const path = require('path');
 const fs = require('fs-extra');
+const less = require('less');
 const specs = require('./specs');
 
 const lesscPath = require.resolve('.bin/lessc');
@@ -16,13 +17,13 @@ const cssPath = path.resolve(fixturesPath, 'css');
 let ids = specs.getIds().filter(v => !/^fail-/.test(v));
 
 ids.forEach(id => {
-  const less = path.join(lessPath, `${id}.less`);
+  const lessFile = path.join(lessPath, `${id}.less`);
   const css = path.join(cssPath, `${id}.css`);
   const tmpless = path.join(lessPath, `${id}.tmp.less`);
 
   let replaces = specs.getReplacements(id);
 
-  let content = fs.readFileSync(less, 'utf-8');
+  let content = fs.readFileSync(lessFile, 'utf-8');
 
   replaces.forEach(r => {
     content = content.replace(r[0], r[1]);
@@ -31,7 +32,12 @@ ids.forEach(id => {
 
   let options = specs.getLesscOpt(id);
 
-  let cmd = `${lesscPath} --relative-urls ${options} ${tmpless} ${css}`;
+  let relativeUrls = '--relative-urls';
+  if (less.version[0] >= 3 && less.version[1] >=8 ) {
+    // https://github.com/less/less.js/blob/59e919b3fc968a403405e39cf15237936b1a6b46/bin/lessc#L479-L484
+    relativeUrls = '--rewrite-urls=all';
+  }
+  let cmd = `${lesscPath} ${relativeUrls} ${options} ${tmpless} ${css}`;
 
   console.log(`Generate spec: ${id}`);
 
