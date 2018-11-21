317cc2384b5d643d3789e9ab69ab7831eec7f670
Gcaufy <gcaufy@gmail.com>
2018-11-21 13:39:44 +0800

    fix(cli): added warn for es6 option

--- packages/wepy-async-function/index.js
+++ packages/wepy-async-function/index.js
@@ -10,10 +10,15 @@
 
 var g = require('./global');
 
-if (!g.Promise) {
-  // IOS 10.0.1 may cause IOS crash.
-  g.Promise = require('promise-polyfill');
+if (!g) {
+  console.warn('请确认关闭小程序选项 "关闭ES6转ES5"');
+} else {
+  if (!g.Promise) {
+    // IOS 10.0.1 may cause IOS crash.
+    g.Promise = require('promise-polyfill');
+  }
+  if (!g.regeneratorRuntime) {
+    g.regeneratorRuntime = require('regenerator-runtime/runtime');
+  }
 }
-if (!g.regeneratorRuntime) {
-  g.regeneratorRuntime = require('regenerator-runtime/runtime');
-}
+
