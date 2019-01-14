b1c06147342da2ec36f41b2f86d91e115ad35b4f
dlhandsome <505441575@qq.com>
2019-01-14 21:26:09 +0800

    feat(core): support multiple global mixin

--- packages/core/weapp/apis/mixin.js
+++ packages/core/weapp/apis/mixin.js
@@ -1,18 +1,5 @@
 import $global from '../global';
-import { isPlainObject } from '../util/index';
 
 export function mixin (options = {}) {
-  if (isPlainObject(options)) {
-    $global.mixin = options;
-  } else {
-    console.error(
-      'Mixin global api supports plain object only\n\n' +
-      'e.g: \n\n' +
-      'wepy.mixin({\n' +
-      '  created () {\n' +
-      '    console.log(\'global mixin created\')\n' +
-      '  }\n' +
-      '})'
-    )
-  }
+  $global.mixin = ($global.mixin || []).concat(options);
 }
