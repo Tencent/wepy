4a8435bdbf6e82334493bfc2a4755a4720109bc7
Gcaufy <gcaufy@gmail.com>
2019-01-31 21:25:08 +0800

    feat: update auto publish code

--- scripts/npm_publish/index.js
+++ scripts/npm_publish/index.js
@@ -7,33 +7,29 @@ const writePkg = require('write-pkg');
 const autoPublish = require('./lib/auto');
 const interactPublish = require('./lib/interact');
 const isGitWorkTreeClean = require('./lib/check_repo_clean');
+const log = require('./lib/log');
 
 
 isGitWorkTreeClean().then(() => {
   commander.command('publish [name]')
   .option('-t, --tag <tag>', 'publish tag')
-  .option('-v, --version <version>', 'publish version')
+  .option('-v, --version <ver>', 'publish version')
+  .option('-i, --increase <increase>', 'version increase')
   .option('-q, --no-interact', 'with out interact')
   .action(function (name, opt) {
     name = name || '*';
 
-    if (name !== '*') {
-      if (opt.interact) {
-        interactPublish(name, opt);
-      } else {
-        autoPublish(name, opt);
-      }
+    if (opt.interact) {
+      interactPublish(name, opt);
+    } else {
+      autoPublish(name, opt);
     }
   });
 
   commander.parse(process.argv);
 }).catch(e => {
-  if (e.message === 'not clean') {
-    console.error('ERR: Work tree is not clean, publish process stopped.');
-  } else {
-    console.error(e);
-  }
-})
+  log.error(e.message);
+});
 
 
 
