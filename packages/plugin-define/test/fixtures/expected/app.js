6738858fc740fbab8a152e45982aca99c2a53a55
Gcaufy <gcaufy@gmail.com>
2019-05-27 19:38:47 +0800

    test(plugin-define): added test cases

---
+++ packages/plugin-define/test/fixtures/expected/app.js
@@ -0,0 +1,12 @@
+module.exports = {
+  definedVariable: 'http://www.foo.com',
+  inObjectProperty: { BASE_URL: 'http://foo.com' },
+  inString: 'BASE_URL',
+  inTemplateLiterals: 'http://www.bar.com/api/',
+
+  shouldReplaced: 'http://www.bar.com',
+  objectTest: 'development',
+
+  typeofTest: 'undefined',
+  typeofTest2: 'undefined'
+};
