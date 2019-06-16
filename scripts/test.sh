b0ba1bfd227956b662669676257fba6c1900e0b6
Gcaufy <gcaufy@gmail.com>
2019-06-16 21:00:12 +0800

    fix: removed json test

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,8 +7,5 @@ if [ "$TEST_DEBUG" ]; then
    node="node --inspect --debug-brk"
 fi
 
-node ./node_modules/jsonlint/lib/cli.js -q ./docs/data/cases.json && echo 'cases.json ok.'
-node ./node_modules/jsonlint/lib/cli.js -q ./docs/data/donate.json && echo 'donate.json ok.'
-
 node ./node_modules/eslint/bin/eslint.js packages/*
 node ./node_modules/istanbul/lib/cli.js cover ./node_modules/mocha/bin/_mocha -- -t 50000 --recursive  -R spec packages/*/test/
