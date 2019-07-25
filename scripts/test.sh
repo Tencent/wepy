cf25e575ef56e624074be044cea5704cfcb8e289
Gcaufy <gcaufy@gmail.com>
2019-07-25 17:34:48 +0800

    test: added core test in main test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,7 +7,7 @@ cd packages/compiler-less && npm run test && cd ../../
 cd packages/compiler-sass && npm run test && cd ../../
 # TODO: cd packages/compiler-stylus && npm run test && cd ../../
 # TODO: cd packages/compiler-typescript && npm run test && cd ../../
-# TODO: cd packages/core && npm run test && cd ../../
+cd packages/core && npm run test:types && npm run test && cd ../../
 cd packages/plugin-define && npm run test && cd ../../
 # TODO: cd packages/plugin-eslint && npm run test && cd ../../
 # TODO: cd packages/plugin-uglifyjs && npm run test && cd ../../
