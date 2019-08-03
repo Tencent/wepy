18bc3a8fcf0b5f5ae26f0c7126f360338aa7d0a0
kino <nishinotsukasavirgo@gmail.com>
2019-08-03 19:21:07 +0800

    refactor(redux): 重构 redux，优化性能，将其包含进 observer 里，可以被 watch 以及被其它 computed 依赖

--- packages/redux/install.js
+++ packages/redux/install.js
@@ -9,22 +9,60 @@ export default function wepyInstall (wepy) {
       } else if (options.parent && options.parent.$store) {
         this.$store = options.parent.$store;
       }
-    },
-    created () {
-      const store = this.$store;
-      if (store) {
-        const computed = this.$options.computed;
-        store.subscribe(() => {
-          for (let k in computed) {
-            if (computed[k].redux) {
-              // Set the key to dirty
-              this._computedWatchers[k].evaluate();
-              // Force render
-              this.$forceUpdate();
-            }
+      if (checkReduxComputed(this.$options)) {
+        if (!this.$store) {
+          console.warn(`[@wepy/redux] state do not work, if store is not defined.`);
+          return;
+        }
+        const { computed } = this.$options;
+        const keys = Object.keys(computed);
+        for (let i = 0; i < keys.length; i++) {
+          if ('resValueMap' in computed[keys[i]]) {
+            wepy.observe({
+              vm: this,
+              key: '',
+              value: computed[keys[i]].resValueMap,
+              parent: '',
+              root: true
+            });
+            break;
           }
-        });
+        }
       }
+    },
+
+    created () {
+      if (!checkReduxComputed(this.$options)) {
+        return;
+      }
+      const store = this.$store;
+      const { computed } = this.$options;
+      this.$unsubscribe = store.subscribe(() => {
+        const keys = Object.keys(computed);
+        for (let i = 0; i < keys.length; i++) {
+          if ('redux' in computed[keys[i]]) {
+            this._computedWatchers[keys[i]].get();
+          }
+        }
+      });
+    },
+
+    detached () {
+      this.$unsubscribe && this.$unsubscribe();
     }
   })
 };
+
+function checkReduxComputed(options) {
+  if (!('computed' in options)) {
+    return false;
+  }
+  const { computed } = options;
+  const keys = Object.keys(computed);
+  for (let i = 0; i < keys.length; i++) {
+    if ('redux' in computed[keys[i]]) {
+      return true;
+    }
+  }
+  return false;
+}
