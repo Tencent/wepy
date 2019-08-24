1a65fbfab9fa0a394e1c7c6033f200bc92d66e4d
Gcaufy <gcaufy@gmail.com>
2019-08-24 14:44:14 +0800

    build(redux): build dist

--- packages/redux/dist/index.js
+++ packages/redux/dist/index.js
@@ -11,22 +11,32 @@ function normalizeMap (map) {
 }
 
 var mapState = function (states) {
-  var res = {};
+  var res = Object.create(null);
+  var resValueMap = Object.create(null);
   normalizeMap(states).forEach(function (ref) {
     var key = ref.key;
     var val = ref.val;
 
+    /**
+     * resValueMap 记录由 mapState 产生的值
+     * 在初始化时将其变成 observer 的
+     */
+    resValueMap[key] = Object.preventExtensions({ value: undefined });
     res[key] = function mappedState() {
-      if (!this.$store) {
-        console.warn(("[@wepy/redux] state \"" + key + "\" do not work, if store is not defined."));
-        return;
-      }
       var state = this.$store.getState();
-      return typeof val === 'function'
+      var value = typeof val === 'function'
         ?  val.call(this, state)
         : state[val];
+
+      // 利用 redux state 每次改变都会返回一个新 state 的特性，只需做引用比较
+      var resValueMap = res[key][this.$id];
+      if (resValueMap[key].value !== value) {
+        resValueMap[key] = Object.preventExtensions({ value: value });
+      }
+      return resValueMap[key].value;
     };
     res[key].redux = true;
+    res[key].resValueMap = resValueMap;
   });
   return res;
 };
@@ -63,6 +73,8 @@ var mapActions = function (actions) {
 function wepyInstall (wepy) {
   wepy.mixin({
     beforeCreate: function beforeCreate () {
+      var this$1 = this;
+
       var options = this.$options;
       if (options.store) {
         this.$store = typeof options.store === 'function'
@@ -71,26 +83,69 @@ function wepyInstall (wepy) {
       } else if (options.parent && options.parent.$store) {
         this.$store = options.parent.$store;
       }
+      if (checkReduxComputed(this.$options)) {
+        if (!this.$store) {
+          console.warn("[@wepy/redux] state do not work, if store is not defined.");
+          return;
+        }
+        var ref = this.$options;
+        var computed = ref.computed;
+        var keys = Object.keys(computed);
+        var resValueMap;
+        for (var i = 0; i < keys.length; i++) {
+          if ('resValueMap' in computed[keys[i]]) {
+            if (!resValueMap) {
+              resValueMap = Object.assign({}, computed[keys[i]].resValueMap);
+            }
+            computed[keys[i]][this$1.$id] = resValueMap;
+          }
+        }
+        wepy.observe({
+          vm: this,
+          key: '',
+          value: resValueMap,
+          parent: '',
+          root: true
+        });
+      }
     },
+
     created: function created () {
       var this$1 = this;
 
-      var store = this.$store;
-      if (store) {
-        var computed = this.$options.computed;
-        store.subscribe(function () {
-          for (var k in computed) {
-            if (computed[k].redux) {
-              // Set the key to dirty
-              this$1._computedWatchers[k].evaluate();
-              // Force render
-              this$1.$forceUpdate();
-            }
-          }
-        });
+      if (!checkReduxComputed(this.$options)) {
+        return;
       }
+      var store = this.$store;
+      var ref = this.$options;
+      var computed = ref.computed;
+      this.$unsubscribe = store.subscribe(function () {
+        var keys = Object.keys(computed);
+        for (var i = 0; i < keys.length; i++) {
+          if ('redux' in computed[keys[i]]) {
+            this$1._computedWatchers[keys[i]].get();
+          }
+        }
+      });
+    },
+
+    detached: function detached () {
+      this.$unsubscribe && this.$unsubscribe();
     }
   });
+}
+function checkReduxComputed(options) {
+  if (!('computed' in options)) {
+    return false;
+  }
+  var computed = options.computed;
+  var keys = Object.keys(computed);
+  for (var i = 0; i < keys.length; i++) {
+    if ('redux' in computed[keys[i]]) {
+      return true;
+    }
+  }
+  return false;
 }
 
 var index = {
