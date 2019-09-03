e594bd7aa65ecf36bb9d44a7f6242e3481e77ecb
Gcaufy <gcaufy@gmail.com>
2019-09-03 16:23:04 +0800

    fix(cli): keep unregister function

--- packages/cli/core/hook.js
+++ packages/cli/core/hook.js
@@ -99,6 +99,18 @@ class Hook {
     return (args.length <= 1 ? args[0] : args);
   }
 
+  unregister (key, fn) {
+    let fns = this._hooks[key];
+    if (fns && typeof fn === 'function') {
+      fns = fns.filter(f => f !== fn);
+      if (fns.length > 0) {
+        this._hooks[key] = fns;
+      } else {
+        delete this._hooks[key];
+      }
+    }
+  }
+
   unregisterAll (key) {
     delete this._hooks[key];
   }
