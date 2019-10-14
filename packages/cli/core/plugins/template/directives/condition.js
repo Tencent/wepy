576890e58f7993cdb80a55018c411d241767704b
SpringHgui <740360381@qq.com>
2019-10-14 16:12:45 +0800

    fix: 解析wx:if等为a:if以兼容支付宝小程序

--- packages/cli/core/plugins/template/directives/condition.js
+++ packages/cli/core/plugins/template/directives/condition.js
@@ -5,10 +5,23 @@ const ADDITIONS_DIRECTIVES_HANDLES = {
   'v-else': ({item, name, expr}) => ({attrs: { 'wx:else': true }})
 };
 
+const ADDITIONS_DIRECTIVES_HANDLES_ANT = {
+  'v-show': ({item, name, expr}) => ({attrs: { hidden: `{{ !(${expr}) }}` }}),
+  'v-if': ({item, name, expr}) => ({attrs: { 'a:if': `{{ ${expr} }}` }}),
+  'v-else-if': ({item, name, expr}) => ({attrs: { 'a:elif': `{{ ${expr} }}` }}),
+  'v-else': ({item, name, expr}) => ({attrs: { 'a:else': true }}),
+  
+  'wx:if': ({item, name, expr}) => ({attrs: { 'a:if': `${expr}` }}),
+};
+
 exports = module.exports = function () {
+  let items = ADDITIONS_DIRECTIVES_HANDLES;
+  if(this.options.output === 'ant')
+  {
+    items = ADDITIONS_DIRECTIVES_HANDLES_ANT;
+  }
 
-  Object.keys(ADDITIONS_DIRECTIVES_HANDLES).forEach(directives => {
-
-    this.register('template-parse-ast-attr-' + directives, ADDITIONS_DIRECTIVES_HANDLES[directives]);
+  Object.keys(items).forEach(directives => {
+    this.register('template-parse-ast-attr-' + directives, items[directives]);
   });
 };
