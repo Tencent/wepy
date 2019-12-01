37152ca74d31e0c58859f2ff509a66ec3b8309be
dlhandsome <505441575@qq.com>
2019-12-01 19:31:26 +0800

    fix(cli): fixed sfcCustomBlock

---
+++ packages/cli/test/core/plugins/helper/sfcCustomBlock.test.js
@@ -0,0 +1,54 @@
+const { alias } = require('../../../config');
+const sfcCompiler = require('vue-template-compiler');
+const Hook = require(`${alias.core}/hook`);
+const initPlugin = require(`${alias.core}/init/plugin`);
+
+const expect = require('chai').expect;
+
+function createCompile() {
+  const hook = new Hook();
+  hook.options = { plugins: [] };
+  initPlugin(hook);
+
+  return hook;
+}
+
+describe('sfcCustomBlock', function() {
+  const compiler = createCompile();
+
+  it('test default lang', function() {
+    const sfc = sfcCompiler.parseComponent(
+      `
+      <config></config>
+      <wxs></wxs>
+    `,
+      { pad: 'space' }
+    );
+
+    const parsedSfc = compiler.hookSeq('sfc-custom-block', sfc);
+
+    expect(parsedSfc).to.have.a.property('config');
+    expect(parsedSfc).to.have.a.property('wxs');
+    expect(parsedSfc.config.lang).to.equal('json');
+    expect(parsedSfc.wxs).to.be.an.instanceof(Array);
+  });
+
+  it('test custom lang', function() {
+    const sfc = sfcCompiler.parseComponent(
+      `
+      <config lang="customLang1"></config>
+      <wxs lang="customLang2"></wxs>
+      <wxs></wxs>
+    `,
+      { pad: 'space' }
+    );
+
+    const parsedSfc = compiler.hookSeq('sfc-custom-block', sfc);
+
+    expect(parsedSfc).to.have.a.property('config');
+    expect(parsedSfc).to.have.a.property('wxs');
+    expect(parsedSfc.config.lang).to.equal('customLang1');
+    expect(parsedSfc.wxs).to.have.lengthOf(2);
+    expect(parsedSfc.wxs.some(_ => _.lang === 'customLang2')).to.be.true;
+  });
+});
