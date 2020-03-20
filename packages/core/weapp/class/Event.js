ca8e4222308e042db13b003f4337dfe079962a41
kino <nishinotsukasavirgo@gmail.com>
2020-03-20 21:36:46 +0800

    refactor(core): 撤回对 Event 的修改

--- packages/core/weapp/class/Event.js
+++ packages/core/weapp/class/Event.js
@@ -4,17 +4,9 @@ export default class Event {
     this.$wx = e;
     this.type = e.type;
     this.timeStamp = e.timeStamp;
-    this.detail = detail;
-
     if (detail) {
       this.x = detail.x;
       this.y = detail.y;
-
-      if ('arguments' in detail) {
-        this.detail = detail.arguments.length > 1 ? detail.arguments : detail.arguments[0];
-        this.arguments = detail.arguments;
-        e.detail = this.detail;
-      }
     }
 
     this.target = target;
