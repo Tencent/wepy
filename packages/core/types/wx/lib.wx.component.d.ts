37ce1fcf84218d59f4cb3d9354938b35fe879fec
Fu Diwei <fudiwei@sina.com>
2020-03-30 10:22:37 +0800

    fix(core): ts reference path

--- packages/core/types/wx/lib.wx.component.d.ts
+++ packages/core/types/wx/lib.wx.component.d.ts
@@ -1,1 +1,4 @@
+// @ts-ignore
+/// <reference path="../../node_modules/miniprogram-api-typings/types/wx/lib.wx.component.d.ts" />
+// @ts-ignore
 /// <reference path="../../node_modules/miniprogram-api-typings/types/wx/lib.wx.component.d.ts" />
