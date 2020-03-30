386af33b20486f2248bb0b9e835c31de84c5b122
Fu Diwei <fudiwei@sina.com>
2020-03-30 10:35:06 +0800

    fix(core): remove useless ts declaration

--- packages/core/types/wx/lib.wx.page.d.ts
+++ packages/core/types/wx/lib.wx.page.d.ts
@@ -43,7 +43,7 @@ declare namespace wepy {
        *
        * 其中 `key` 可以以数据路径的形式给出，支持改变数组中的某一项或对象的某个属性，如 `array[2].message`，`a.b.c.d`，并且不需要在 this.data 中预先定义。
        */
-      data: D | Pick<D, K> | IAnyObject,
+      data: D | Pick<D, K> | WechatMiniprogram.IAnyObject,
       /** setData引起的界面更新渲染完毕后的回调函数，最低基础库： `1.5.0` */
       callback?: () => void
     ): void
