de312dc8efaa909479ec39d2147a70116aa63118
Fu Diwei <fudiwei@sina.com>
2021-02-05 19:13:40 +0800

    feat(core): rewrite TS declaration

--- packages/core/types/wx/lib.wx.component.d.ts
+++
@@ -1,4 +0,0 @@
-// @ts-ignore
-/// <reference path="../../node_modules/miniprogram-api-typings/types/wx/lib.wx.component.d.ts" />
-// @ts-ignore
-/// <reference path="../../node_modules/miniprogram-api-typings/types/wx/lib.wx.component.d.ts" />
